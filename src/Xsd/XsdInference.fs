e2d2039644e43422645fdbd547477c0d098c95ce
giacomociti <giacomociti@gmail.com>
2015-08-02 21:12:27 +0200

    XmlProviderFromSchema

---
+++ src/Xsd/XsdInference.fs
@@ -0,0 +1,201 @@
+﻿namespace ProviderImplementation
+
+open System.Xml
+open System.Xml.Schema
+
+module XsdModel =
+
+    type XsdElement = { Name: XmlQualifiedName; Type: XsdType; IsNillable: bool }
+
+    and XsdType = SimpleType of XmlTypeCode | ComplexType of XsdComplexType
+
+    and XsdComplexType = 
+        { Attributes: (XmlQualifiedName * XmlTypeCode * bool) list // bool is for optional
+          Contents: XsdContent 
+          IsMixed: bool }
+
+    and XsdContent = SimpleContent of XmlTypeCode | ComplexContent of XsdParticle
+
+    and Occurs = decimal * decimal
+
+    and XsdParticle = 
+        | Empty
+        | Any      of Occurs 
+        | Element  of Occurs * XsdElement
+        | All      of Occurs * XsdParticle list
+        | Choice   of Occurs * XsdParticle list
+        | Sequence of Occurs * XsdParticle list
+
+module XsdParsing =
+    
+    open XsdModel    
+    open Microsoft.FSharp.Core
+
+    let inline ofType<'a> sequence = System.Linq.Enumerable.OfType<'a> sequence
+
+    let rec parseElement (elm: XmlSchemaElement) =  
+        
+        let rec parseParticle (par: XmlSchemaParticle) =
+
+            let occurs = par.MinOccurs, par.MaxOccurs
+
+            let parseParticles (group: XmlSchemaGroupBase) =
+                let particles = 
+                    group.Items
+                    |> ofType<XmlSchemaParticle> 
+                    |> Seq.map parseParticle
+                    |> List.ofSeq // beware of recursive schemas
+                match group with
+                | :? XmlSchemaAll      -> All (occurs, particles)
+                | :? XmlSchemaChoice   -> Choice (occurs, particles)
+                | :? XmlSchemaSequence -> Sequence (occurs, particles)
+                | _ -> failwithf "unknown group base: %A" group
+
+            match par with
+            | :? XmlSchemaAny -> Any occurs
+            | :? XmlSchemaGroupBase as grp -> parseParticles grp
+            | :? XmlSchemaGroupRef as grpRef -> parseParticle grpRef.Particle
+            | :? XmlSchemaElement as elm -> Element (occurs, parseElement elm)
+            | _ -> Empty // XmlSchemaParticle.EmptyParticle
+
+        { Name = elm.QualifiedName
+          Type = 
+            match elm.ElementSchemaType with
+            | :? XmlSchemaSimpleType  as x -> SimpleType x.Datatype.TypeCode
+            | :? XmlSchemaComplexType as x -> 
+                ComplexType
+                    { Attributes = 
+                        x.AttributeUses.Values |> ofType<XmlSchemaAttribute>
+                        |> Seq.filter (fun a -> a.Use <> XmlSchemaUse.Prohibited)
+                        |> Seq.map (fun a -> a.QualifiedName,
+                                             a.AttributeSchemaType.Datatype.TypeCode, 
+                                             a.Use <> XmlSchemaUse.Required)
+                        |> List.ofSeq
+                      Contents = 
+                          match x.ContentType with
+                            | XmlSchemaContentType.TextOnly -> SimpleContent x.Datatype.TypeCode
+                            | XmlSchemaContentType.Mixed 
+                            | XmlSchemaContentType.Empty 
+                            | XmlSchemaContentType.ElementOnly -> 
+                                x.ContentTypeParticle |> parseParticle |> ComplexContent
+                            | _ -> failwith "Unknown content type: %A." x.ContentType
+
+                      IsMixed = x.IsMixed }
+            | x -> failwithf "unknown ElementSchemaType: %A" x
+          IsNillable = elm.IsNillable }
+
+    let createSchemaSet (xmlReader: XmlReader) =
+        let schemaSet = new XmlSchemaSet()
+        use reader = xmlReader
+        XmlSchema.Read(reader, null) |> schemaSet.Add |> ignore
+        schemaSet.Compile()
+        schemaSet
+
+    let createSchemaSetFromText xsdText = 
+        new XmlTextReader(new System.IO.StringReader(xsdText))
+        |> createSchemaSet
+
+    let createSchemaSetFromUri schemaUri =
+        XmlReader.Create(inputUri = schemaUri, settings = XmlReaderSettings())
+        |> createSchemaSet
+    
+
+    let getElement elmName elmNs (schema: XmlSchemaSet) =
+        schema.GlobalElements.Values 
+        |> ofType<XmlSchemaElement>
+        |> Seq.tryFind (fun x -> 
+            x.QualifiedName.Name = elmName && 
+            x.QualifiedName.Namespace = elmNs)
+        |> Option.map parseElement
+        |> function 
+            | None -> failwithf "No element found with name '%s' and namespace '%s'." elmName elmNs
+            | Some e -> e
+
+
+    let getElement' xsdText elmName elmNs =
+        xsdText 
+        |> createSchemaSetFromText
+        |> fun x -> x.GlobalElements.Values
+        |> ofType<XmlSchemaElement>
+        |> Seq.find (fun x -> 
+            x.QualifiedName.Name = elmName && 
+            x.QualifiedName.Namespace = elmNs)
+        |> parseElement
+
+
+module XsdInference =
+    open XsdModel
+    open FSharp.Data.Runtime.StructuralTypes
+
+    let mult = function
+        | 1M, 1M -> InferedMultiplicity.Single
+        | 0M, 1M -> InferedMultiplicity.OptionalSingle
+        | _ -> InferedMultiplicity.Multiple
+
+    let getType = function
+        | XmlTypeCode.Int -> typeof<int>
+        | XmlTypeCode.Date -> typeof<System.DateTime>
+        | XmlTypeCode.Boolean -> typeof<bool>
+        | XmlTypeCode.Decimal -> typeof<decimal>
+        // etc..
+        | _ -> typeof<string>
+
+    let rec inferElement elm =
+        
+        let rec inferParticle = function
+            | Element (occurs, e) -> 
+                InferedTypeTag.Record(Some e.Name.Name), 
+                (mult occurs, inferElement e)
+            | All (occurs, items) 
+            | Sequence (occurs, items) -> 
+                let inner = items |> List.map inferParticle
+                let tags  = inner |> List.map fst
+                let types = inner |> Map.ofList
+                InferedTypeTag.Collection, 
+                (mult occurs, InferedType.Collection(tags, types))
+            | Choice (occurs, items) ->  
+                let makeOptional = function Single -> OptionalSingle | x -> x
+                let inner = items |> List.map inferParticle
+                let tags  = inner |> List.map fst
+                let types = 
+                    inner 
+                    |> List.map (fun (tag, (mult, ty)) -> tag, (makeOptional mult, ty))
+                    |> Map.ofList
+                InferedTypeTag.Collection, 
+                (mult occurs, InferedType.Collection(tags, types))
+            | Empty -> 
+                InferedTypeTag.Collection,
+                (InferedMultiplicity.OptionalSingle, InferedType.Top)
+            | Any _ -> 
+                let name = Some "{anyNs}anyElement"
+                InferedTypeTag.Record(name), 
+                (InferedMultiplicity.Multiple, 
+                 InferedType.Record (name, [{Name = ""; Type = InferedType.Top}], false))
+                
+
+        let name = Some elm.Name.Name
+        match elm.Type with
+        | SimpleType typeCode ->
+            let body: InferedProperty =
+                { Name = ""
+                  Type = InferedType.Primitive (getType typeCode, None, elm.IsNillable) }
+            InferedType.Record(name, [body], optional = false)
+                  
+        | ComplexType c -> 
+            let attrs: InferedProperty list = 
+                c.Attributes
+                |> List.map (fun (name, typeCode, optional) ->
+                    { Name = name.Name
+                      Type = InferedType.Primitive (getType typeCode, None, optional) } )
+
+            match c.Contents with
+            | SimpleContent typeCode -> 
+                let body: InferedProperty = 
+                    { Name = ""
+                      Type = InferedType.Primitive (getType typeCode, None, elm.IsNillable) }
+                InferedType.Record(name, body::attrs, optional = false)
+
+            | ComplexContent xsdParticle ->
+                let _,(_, ty) = inferParticle xsdParticle
+                let body: InferedProperty = { Name = ""; Type = ty }
+                InferedType.Record(name, body::attrs, optional = false)
